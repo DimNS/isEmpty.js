1ac3f9eac72da39d95b23742d56a4eca3aee9f5b
Дмитрий Щербаков <atomcms@ya.ru>
2019-07-15 17:43:43 +0500

    Gulp upgraded to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,19 @@ var gulp   = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', ['build', 'watcher.js']);
-
-gulp.task('build', ['js', 'js.min']);
-
-gulp.task('watcher.js', function () {
-    return gulp.watch('src/*.js', ['build']);
-});
-
-gulp.task('js', function () {
+function JS() {
     return gulp.src('src/isEmpty.js')
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('js.min', function () {
+function minJS() {
     return gulp.src('src/isEmpty.js')
         .pipe(uglify())
         .pipe(rename('isEmpty.min.js'))
         .pipe(gulp.dest('dist'));
-});
+}
+
+gulp.task('build', gulp.series(
+    JS,
+    minJS
+));
